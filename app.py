5d87b37836841e15e0d5118f367ed370f34d5bc6
Kanchan-Baran-Hazra <kanchanbhazra7063@gmail.com>
2025-08-22 17:36:32 +0530

    Updated project - resubmission

--- app.py
+++ app.py
@@ -30,4 +30,6 @@ def not_placed():
     return render_template('not_placed.html')
 
 if __name__=="__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Railway gives port dynamically
+    app.run(host="0.0.0.0", port=port, debug=True)
